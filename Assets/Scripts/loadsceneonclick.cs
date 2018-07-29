ae1db2369f04f9f8d5ea9989f3dfb8025066b8d2
rickerg <rickerg0@gmail.com>
2018-07-29 19:41:41 -0400

    added menus items, third image target
    added menus items

---
+++ Assets/Scripts/loadsceneonclick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class loadsceneonclick : MonoBehaviour
+{
+
+	 public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
